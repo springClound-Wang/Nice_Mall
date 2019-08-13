453c3c305a7b5264214183a9cd37864cb74ac4cd
ltt <823030735@qq.com>
2019-08-13 11:08:16 +0800

    空壳

---
+++ nice-mall-backend/src/main/java/com/fdj/nicemallbackend/NiceMallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fdj.nicemallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceMallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceMallBackendApplication.class, args);
+	}
+
+}
---
+++ nice-mall-backend/src/test/java/com/fdj/nicemallbackend/NiceMallBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fdj.nicemallbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NiceMallBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
